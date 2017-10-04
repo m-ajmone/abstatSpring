b0ad93346c9649b9d0b8401866ef118de2f7b520
Renzo Alva Principe <r.alvaprincipe@campus.unimib.it>
2017-10-04 12:04:44 +0200

    added Solr indexing script

---
+++ pipeline/export-to-solr.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function as_absolute()
+{
+	echo `cd $1; pwd`
+}
+
+function run(){
+	echo "$@"
+	java -Xms256m -Xmx16g -cp .:'summarization.jar' com.summarization.export.$@
+	echo "Done"
+}
+
+set -e
+relative_path=`dirname $0`
+current_directory=$(as_absolute $relative_path)
+cd $current_directory
+
+echo
+echo "Indexing the produced summary"
+
+dataset=$1
+payleveldomain=$2
+
+cd ../summarization-spring
+
+run DeleteAllDocumentsFromIndex $dataset
+
+sleep 1
+
+run RunResourcesIndexing ../data/summaries/$dataset/patterns/count-concepts.txt $dataset concept $payleveldomain
+run RunResourcesIndexing ../data/summaries/$dataset/patterns/count-datatype.txt $dataset datatype $payleveldomain
+run RunResourcesIndexing ../data/summaries/$dataset/patterns/count-datatype-properties.txt $dataset datatypeProperty $payleveldomain
+run RunResourcesIndexing ../data/summaries/$dataset/patterns/count-object-properties.txt $dataset objectProperty $payleveldomain
+run RunAKPIndexing ../data/summaries/$dataset/patterns/datatype-akp.txt $dataset datatypeAkp $payleveldomain
+run RunAKPIndexing ../data/summaries/$dataset/patterns/object-akp.txt $dataset objectAkp $payleveldomain
+
+cd ../pipeline
